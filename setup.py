4fb878a08bea11e2e4416029a95b41bb8416ddaa
Lucas <Lucasgronlund@gmail.com>
2021-03-13 16:32:09 +0100

    add readme to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
+import pathlib
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+README = (HERE / "README.md").read_text()
 
 setup(
     name='printolog',
     version='0.1',
     description='Python decorator that makes functions use a logger instead of print.',
+    long_description=README,
+    long_description_content_type="text/markdown",
     packages=['printolog'],
     license='MIT',
     author='Lucas Grönlund',
